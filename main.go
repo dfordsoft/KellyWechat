0f1b09059b51d6c5cf5444fd91405bf09a17777a
Fan Yang <missdeer@gmail.com>
2014-10-22 21:42:53 +0800

    (*)add more routers

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	wxmp := new(controllers.WXMPController)
 	beego.Router("/", wxmp, "get:Get;post:Post")
+	beego.Router("/wd/add", wxmp, "post:Post")
+	beego.Router("/article/add", wxmp, "post:Post")
 	beego.HttpPort = 8091
 	beego.Run()
 }
