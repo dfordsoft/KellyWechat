2540ecf1c074fe91302305a9f4051eeb46a2c175
Fan Yang <missdeer@dfordsoft.com>
2014-10-22 14:21:41 +0800

    (*)change help content

--- controllers/default.go
+++ controllers/default.go
@@ -119,8 +119,18 @@ func dealwith(req *Request) (resp *Response, err error) {
     beego.Info(req.MsgType)
     beego.Info(req.Content)
     if req.MsgType == Text {
-        if strings.Trim(strings.ToLower(req.Content), " ") == "help" || req.Content == "Hello2BizUser" || req.Content == "subscribe" {
-            resp.Content = "目前支持包的使用说明及例子的说明，这些例子和说明来自于github.com/astaxie/gopkg，例如如果你想查询strings有多少函数，你可以发送：strings，你想查询strings.ToLower函数，那么请发送：strings.ToLower"
+        userInputText := strings.Trim(strings.ToLower(req.Content), " ")
+        if userInputText == "help" || userInputText == `帮助` {
+            resp.Content = `您好，感谢关注衣丽社区微信公众号i-yiili，希望我们能为您的生活提供一点帮助。
+            我们会通过微信公众号向您不定期地推送一些涉及美容美发化妆、穿着搭配、运动健康、饮食营养、情绪修养等跟女性生活息息相关的文章。
+            同时借助微信公众号强大的可定制性，我们也提供了一系列丰富的功能，并且将不断增加新的实用的功能。
+            输入“文章+日期”或“wz+日期”如（文章20141022 或 wz20141022），显示该天衣丽社区微信公众号发布的文章链接。
+            输入“微店”或“wd”，显示衣丽社区关联的微店地址。
+            输入“女装”或“nz”，显示衣丽社区主持出售的女装商品信息。
+            输入“面膜”或“mm”，显示衣丽社区主持出售的面膜商品信息。
+            输入商品id（如：310148677），显示衣丽社区主持出售的对应商品信息。
+            输入任意关键字，显示包含该关键字的衣丽社区主持出售的商品信息。
+            其他更多功能，敬请期待。`
             return resp, nil
         }
         strs := strings.Split(req.Content, ".")
