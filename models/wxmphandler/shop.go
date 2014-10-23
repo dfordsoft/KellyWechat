9a8be416f9e5176f19f74bdefb5b82960b1c4eed
Fan Yang <missdeer@dfordsoft.com>
2014-10-23 16:02:25 +0800

    (*)query shop info from db

--- models/wxmphandler/shop.go
+++ models/wxmphandler/shop.go
@@ -1,21 +1,29 @@
 package models
 
-func WeiDian(req *Request, resp *Response) error {
-	resp.MsgType = News
-	resp.ArticleCount = 2
-	var a Item
-	a.Description = `女装专卖，经典时尚大方，赶快来看看吧:)`
-	a.Title = `凯莉小姐的梦想女装店，经典时尚大方，适合都市年轻女性，赶快来吧:)`
-	a.PicUrl = "http://wd.geilicdn.com/vshop215091300-1413902752.jpg"
-	a.Url = "http://shopwd.yii.li"
-	resp.Articles = append(resp.Articles, &a)
-	var a2 Item
-	a2.Description = `多款精品面膜，效果超好，输入mm看看吧:)`
-	a2.Title = `清韵诗面膜，多款精品面膜，效果超好，输入mm看看吧:)`
-	a2.PicUrl = "http://wd.geilicdn.com/vshop1015143-1413815285.jpg"
-	a2.Url = "http://wd.koudai.com/s/1015143"
-	resp.Articles = append(resp.Articles, &a2)
+import (
+	"fmt"
+	"github.com/missdeer/KellyWechat/models/wd"
+)
 
+func WeiDian(req *Request, resp *Response) error {
+	qs := models.Shops()
+	shops := make([]models.WDShop, 5)
+	n, err := qs.Limit(5).All(&shops)
+	if err != nil {
+		resp.Content = `没开店哦:(`
+		return nil
+	}
+
+	resp.MsgType = News
+	resp.ArticleCount = int(n)
+	a := make([]Item, n)
+	for i, shop := range shops {
+		a[i].Description = ``
+		a[i].Title = shop.Name + " - " + shop.Note
+		a[i].PicUrl = shop.Logo
+		a[i].Url = fmt.Sprintf(`http://wd.koudai.com/s/%d`, shop.Uuid)
+		resp.Articles = append(resp.Articles, &a[i])
+	}
 	resp.FuncFlag = 1
 	return nil
 }
