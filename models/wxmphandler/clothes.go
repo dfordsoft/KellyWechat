71b82bde72a1cf0851788ef3ff810a62f0a3c43c
Fan Yang <missdeer@dfordsoft.com>
2014-10-23 21:54:43 +0800

    (*)display clothes items randomly

--- models/wxmphandler/clothes.go
+++ models/wxmphandler/clothes.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/missdeer/KellyWechat/models/wd"
+	"math/rand"
+	"time"
 )
 
 func Clothes(req *Request, resp *Response) error {
 	shopId, _ := beego.AppConfig.Int("yf_shop_id")
 	qs := models.Items()
-	items := make([]models.WDItem, 10)
-	n, err := qs.Limit(10).Filter("shop_id", shopId).All(&items)
+	var items []models.WDItem
+	rand.Seed(time.Now().UnixNano())
+	n, err := qs.Limit(500).Filter("shop_id", shopId).All(&items)
 	if err != nil || n == 0 {
 		resp.Content = `没开店哦:(`
 		return nil
@@ -18,12 +21,19 @@ func Clothes(req *Request, resp *Response) error {
 
 	resp.MsgType = News
 	resp.ArticleCount = int(n)
-	a := make([]WXMPItem, n)
-	for i, item := range items {
+	arrayLength := int(n)
+	if arrayLength < 5 {
+		arrayLength++
+	} else {
+		arrayLength = 6
+	}
+	a := make([]WXMPItem, arrayLength)
+	for i := 0; i <= 5; i++ {
 		if n > 6 && i >= 5 {
 			resp.ArticleCount = 5
 			break
 		}
+		item := items[rand.Intn(len(items))]
 		a[i].Description = ``
 		a[i].Title = item.Name
 		a[i].PicUrl = item.Logo
@@ -31,18 +41,16 @@ func Clothes(req *Request, resp *Response) error {
 		resp.Articles = append(resp.Articles, &a[i])
 	}
 
-	if n > 6 {
-		wdShop := &models.WDShop{}
-		wdShop.Id = shopId
-		if wdShop.Get("id") == nil {
-			resp.ArticleCount++
-			shopItem := &WXMPItem{}
-			shopItem.Description = wdShop.Note
-			shopItem.Title = `宝贝数量较多，请进入微店查看更多 - ` + wdShop.Name
-			shopItem.PicUrl = wdShop.Logo
-			shopItem.Url = fmt.Sprintf(`http://wd.koudai.com/s/%d`, wdShop.Uuid)
-			resp.Articles = append(resp.Articles, shopItem)
-		}
+	wdShop := &models.WDShop{}
+	wdShop.Id = shopId
+	shopItem := &WXMPItem{}
+	if wdShop.Get("id") == nil {
+		resp.ArticleCount++
+		shopItem.Description = wdShop.Note
+		shopItem.Title = `宝贝数量较多，请进入微店查看更多 - ` + wdShop.Name
+		shopItem.PicUrl = wdShop.Logo
+		shopItem.Url = fmt.Sprintf(`http://wd.koudai.com/s/%d`, wdShop.Uuid)
+		resp.Articles = append(resp.Articles, shopItem)
 	}
 	resp.FuncFlag = 1
 	return nil
