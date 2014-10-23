ff9ed887da90c0d084e4aca6ba3459c7244ebdcd
Fan Yang <missdeer@gmail.com>
2014-10-23 21:15:22 +0800

    (*)add shop link

--- models/wxmphandler/clothes.go
+++ models/wxmphandler/clothes.go
@@ -20,11 +20,26 @@ func Clothes(req *Request, resp *Response) error {
 	resp.ArticleCount = int(n)
 	a := make([]WXMPItem, n)
 	for i, item := range items {
+		if i >= 5 {
+			resp.ArticleCount = 5
+			break
+		}
 		a[i].Description = ``
 		a[i].Title = item.Name
 		a[i].PicUrl = item.Logo
 		a[i].Url = fmt.Sprintf(`http://wd.koudai.com/i/%d`, item.Uuid)
 		resp.Articles = append(resp.Articles, &a[i])
+	}
+
+	wdShop := &models.WDShop{}
+	wdShop.Id = shopId
+	if wdShop.Get("id") == nil {
+		resp.ArticleCount++
+		var shopItem WXMPItem
+		shopItem.Description = wdShop.Note
+		shopItem.Title = `宝贝数量较多，请进入微店查看更多 - ` + wdShop.Name
+		shopItem.PicUrl = wdShop.Logo
+		shopItem.Url = fmt.Sprintf(`http://wd.koudai.com/s/%d`, wdShop.Uuid)
 	}
 	resp.FuncFlag = 1
 	return nil
