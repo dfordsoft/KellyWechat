a31fa2a2281b3cb791376ec8cde7926bd87636f4
Fan Yang <missdeer@gmail.com>
2014-10-23 22:19:38 +0800

    (*)query item from db

--- models/wxmphandler/item.go
+++ models/wxmphandler/item.go
@@ -1,18 +1,35 @@
 package models
 
 import (
+	"fmt"
+	"github.com/astaxie/beego"
+	"github.com/missdeer/KellyWechat/models/wd"
+	"strconv"
 	"strings"
 )
 
 func ItemId(req *Request, resp *Response) error {
+
+	userInputText := strings.Trim(strings.ToLower(req.Content), " ")
+	id, err := strconv.ParseInt(userInputText, 10, 64)
+	if err != nil {
+		beego.Error("incorrect input ", userInputText)
+		return nil
+	}
+	item := &models.WDItem{}
+	item.Id = int(id)
+	if item.Get("id") != nil {
+		beego.Error("not found ", userInputText)
+		return nil
+	}
+
+	var a WXMPItem
 	resp.MsgType = News
 	resp.ArticleCount = 1
-	var a WXMPItem
-	a.Description = `女装专卖，经典时尚大方，赶快来看看吧:)`
-	a.Title = `凯莉小姐的梦想女装店，经典时尚大方，适合都市年轻女性，赶快来吧:)`
-	a.PicUrl = "http://wd.geilicdn.com/vshop215091300-1413902752.jpg"
-	userInputText := strings.Trim(strings.ToLower(req.Content), " ")
-	a.Url = "http://wd.koudai.com/item.html?itemID=" + userInputText
+	a.Description = ``
+	a.Title = item.Name
+	a.PicUrl = item.Logo
+	a.Url = fmt.Sprintf(`http://wd.koudai.com/i/%d`, item.Uuid)
 	resp.Articles = append(resp.Articles, &a)
 	resp.FuncFlag = 1
 	return nil
